0e6fb143a065e04ff694abe68115c464debbda15
DiegoTeacher <diego.baratto@ifp.es>
2023-10-04 10:12:39 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/test/InsertToDB.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class InsertToDB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_to_db);
+    }
+
+    public void changeToMain(View view) {
+        Intent nIntent = new Intent(InsertToDB.this, MainActivity.class);
+        startActivity(nIntent);
+    }
+}
