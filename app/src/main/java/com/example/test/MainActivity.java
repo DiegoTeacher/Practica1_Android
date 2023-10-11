d2d7cee5933f4526a6fec3f3df2ad1559b122ec4
DiegoTeacher <diego.baratto@ifp.es>
2023-10-11 13:33:33 +0200

    SELECT done

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonTest = findViewById(R.id.buttonTest);
+       /* Button buttonTest = findViewById(R.id.buttonTest);
         buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "hOLAAAAAA", Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
     }
 
     public void diceAdios(View view) {
